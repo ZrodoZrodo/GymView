60662f119d90094b553ec77f27a577296c845d69
SamSepi0l-H3ll0-Fi3ND <kubafigzal@gmail.com>
2024-01-15 21:06:34 +0100

    Register fix

--- gymView/assets/pages/register.js
+++ gymView/assets/pages/register.js
@@ -5,17 +5,15 @@ import RegisterForm from "../components/registerForm";
 
 export const Register = () => {
   return (
-    <div className="min-h-screen grid grid-none md:grid-cols-2">
+    <div className="h-dvh grid grid-none md:grid-cols-2">
       <div
         style={{
           backgroundImage: `url(${backgroundregister})`,
         }}
-        className=" invisible md:visible bg-center bg-cover order-last relative md:order-none min-h-full"
+        className=" invisible md:visible bg-center bg-cover order-last relative md:order-none h-dvh"
       >
         <div className=" ml-8 absolute inset-x-0 bottom-10">
-          <p className="text-white text-3xl">
-            Start the workout!
-          </p>
+          <p className="text-white text-3xl">Start the workout!</p>
           <p className="underline underline-offset-4 text-sm text-white">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta
             laudantium obcaecati.
@@ -26,7 +24,7 @@ export const Register = () => {
         style={{
           background: "#363636",
         }}
-        className="flex items-center flex-wrap justify-center"
+        className="flex items-center flex-wrap justify-center pb-48"
       >
         <div className=" h-1/2 w-3/4">
           <div
